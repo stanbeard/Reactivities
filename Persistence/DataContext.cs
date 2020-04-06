9763fd274411631dd726b3c0fc3265557f30a4dc
= <=>
2020-04-06 21:07:24 +0200

    Initial commit

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+            .HasData(
+                new Value { Id = 1, Name = "Value!"},
+                new Value { Id = 2, Name = "Value?"},
+                new Value { Id = 3, Name = "Value."}
+            );
+            
+        }
+    }
+}
